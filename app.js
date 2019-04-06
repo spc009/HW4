2346300d5d2d26e4392427e0c93a3667338699c6
SUPATCHA NANAN <supatcha_nanan@cmu.ac.th>
2019-04-06 20:54:59 +0700

    secend

---
+++ app.js
@@ -0,0 +1,81 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+
+    // if(req.url === '/'){
+    //     fs.readFile(path.join(__dirname,'public', 'index.html'),(err,content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, { 'Content-type' : 'text/html'});
+    //         res.end(content);
+    //     });
+    // } else if(req.url === '/gallery'){
+
+    // }else{
+    //     fs.readFile(path.join(__dirname,'public', '404.html'),(err,content) => {
+    //         if(err) throw err;
+    //         res.writeHead(404, { 'Content-Type' : 'text/html'});
+    //         res.end(content);
+    //     });
+    // }
+
+    let filePath = path.join(
+        __dirname,
+        'public',
+        req.url === '/' ? 'index.html' : req.url
+    );
+    let extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
+        case ".woff":
+            contentType = "font/woff"
+            break;
+        case ".woff2":
+            contentType = "font/woff2"
+            break;
+    }
+
+    if (contentType == "text/html" && extname == "") filePath += ".html";
+    console.log(filePath);
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'public', '404.html', (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                }))
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error : ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content, "utf8");
+        }
+    });
+});
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
---
+++ public/CTList.js
@@ -0,0 +1,56 @@
+const myForm = document.querySelector('#my-form');
+const userList = document.querySelector('#users');
+fetchContacts();
+function fetchContacts() {
+    var Contacts = JSON.parse(localStorage.getItem('Contacts'));
+
+    var ContactsResult = document.querySelector('#users');
+
+    ContactsResult.innerHTML = '';
+
+    var str = '<div class="card-deck text-center">';
+
+    for (var i = 0; i < Contacts.length; i++) {
+        var name = Contacts[i].Name;
+        var email = Contacts[i].Email;
+        var subject = Contacts[i].Subject;
+        var msg = Contacts[i].MSG;
+
+        str +=`<tr>`
+            +`<td width="10%"></td>`
+            +`<td>`
+            +`<div class="col-sm-6 col-md-4 col-lg-3">`
+            +`<div class="card mb-4 shadow-sm">`
+            +`<div class="card-header">`
+            +`<h4 class="my-0" >${name}</h4>`
+            +`</div>`
+            +`<div class="card-body" style="width: 200px">`
+            +`<div class="message" >`
+            +`<h6>${subject}</h6>`
+            +`<pre>${msg}</pre>`
+            +`</div>`
+            +`<a href="mailto:${email}">${email}</a>`
+            +`<button onclick="deleteCard('${name}','${subject}')" class="btn" href="#">Delete</a>`
+            +`</div></div></div>
+        </td>
+        <td width="10%"></td>
+    </tr>`
+    }
+    str += '</div>'
+
+    ContactsResult.innerHTML = str;
+}
+
+function deleteCard(name,subject) {
+    var Contacts = JSON.parse(localStorage.getItem('Contacts'));
+
+    for (var i = 0; i < Contacts.length; i++) {
+        if (Contacts[i].Name === name && Contacts[i].Subject === subject) {
+            Contacts.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('Contacts', JSON.stringify(Contacts));
+
+    fetchContacts();
+}
